0fe3cb58288d5fd8a696b2be4c46f54047ec3b0e
jijihohococo <jijihohococo@gmail.com>
2022-04-14 01:37:57 +0630

    add

---
+++ app/Http/Controllers/User/BannerController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Banner;
+class BannerController extends Controller
+{
+    //
+    public function get(){
+        return response()->json([
+            'banners' => Banner::latest('id')->get()
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::get('items',function(){
-	dd("items");
-});
+
 
 Route::get('items/{id}','User\ItemController@show');
+Route::get('banners','User\BannerController@get');
