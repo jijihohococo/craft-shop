19972a8c25e2517cd9162361ce1ee4e71167248c
jijihohococo <jijihohococo@gmail.com>
2022-06-12 12:21:34 +0900

    add

--- public/js/resources_js_admin_views_Item_vue.js
+++ public/js/resources_js_admin_views_Item_vue.js
@@ -546,9 +546,10 @@ __webpack_require__.r(__webpack_exports__);
     }
   },
   created: function created() {
-    this.getItems(1); // checkContentPermission(this.content,'create',this);
-    // checkContentPermission(this.content,'update',this);
-    // checkContentPermission(this.content,'delete',this);
+    this.getItems(1);
+    (0,_helpers_check_js__WEBPACK_IMPORTED_MODULE_13__.checkContentPermission)(this.content, 'create', this);
+    (0,_helpers_check_js__WEBPACK_IMPORTED_MODULE_13__.checkContentPermission)(this.content, 'update', this);
+    (0,_helpers_check_js__WEBPACK_IMPORTED_MODULE_13__.checkContentPermission)(this.content, 'delete', this);
   }
 });
 
