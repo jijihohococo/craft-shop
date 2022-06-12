092d5b79b0e7baa4f4792f4d206184f4602badcd
jijihohococo <jijihohococo@gmail.com>
2022-06-12 10:51:06 +0900

    add

--- resources/js/admin/components/Error.vue
+++ resources/js/admin/components/Error.vue
@@ -1,16 +1,16 @@
 <template>
 	<div class="error-page">
-					<h2 class="headline text-warning">404</h2>
+		<h2 class="headline text-warning">404</h2>
 
-					<div class="error-content">
-						<h3><i class="fas fa-exclamation-triangle text-warning"></i>{{ title }}</h3>
+		<div class="error-content">
+			<h3><i class="fas fa-exclamation-triangle text-warning"></i>{{ title }}</h3>
 
-						<p>
-						{{ description }}
-						</p>
-					</div>
-					<!-- /.error-content -->
-				</div>
+			<p>
+				{{ description }}
+			</p>
+		</div>
+		<!-- /.error-content -->
+	</div>
 </template>
 <script >
 	export default {
