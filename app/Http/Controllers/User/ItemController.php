89545562da79a77b68ab614326ae6108787b08df
jijihohococo <jijihohococo@gmail.com>
2022-07-28 12:37:49 +0900

    add

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -18,4 +18,31 @@ class ItemController extends Controller
             'reviews' => $this->getReviews('item_reviews','item_id',$id)
         ]);
     }
+
+    public function showByCategory(Request $request,$categoryId){
+        return response()->json([
+            'items' => Item::selectItemData()
+            ->where('category_id',$categoryId)
+            ->latest('id')
+            ->paginate(10)
+        ]);
+    }
+
+    public function showBySubcategory(Request $request,$subcategoryId){
+        return response()->json([
+            'items' => Item::selectItemData()
+            ->where('subcategory_id',$subcategoryId)
+            ->latest('id')
+            ->paginate(10)
+        ]);
+    }
+
+    public function showByBrand(Request $request,$brandId){
+        return response()->json([
+            'items' => Item::selectItemData()
+            ->where('brand_id',$brandId)
+            ->latest('id')
+            ->paginate(10)
+        ]);
+    }
 }
