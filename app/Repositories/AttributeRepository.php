a274c593bd29fe6b3f8d355509df1fc165aa2774
Lin Aung <jijihohococo@gmail.com>
2022-08-19 13:17:08 +0900

    fixing and refactoring attribute repository

--- app/Repositories/AttributeRepository.php
+++ app/Repositories/AttributeRepository.php
@@ -7,7 +7,11 @@ use App\Http\Resources\AttributeResource;
 class AttributeRepository implements AttributeRepositoryInterface{
 
 	public function getByContent($content,$id){
-		return AttributeResource::collection(Attribute::getByItemData($content.'_id',$id)->orderBy('name')->get())
+		return AttributeResource::collection(Attribute::getByItemData($content.'_id',$id)->orderBy('name')->get());
+	}
+
+	public function searchByContent($content,$id,$searchData){
+		return AttributeResource::collection(Attribute::getByItemSearch($column.'_id',$id,$searchData)->orderBy('name')->get());
 	}
 
 }
