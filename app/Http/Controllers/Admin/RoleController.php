a293c9dce7981e34a777c940bcced46106683138
Lin Aung <jijihohococo@gmail.com>
2022-08-27 15:18:24 +0900

    refactor

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\{Role,RolePermission};
+use App\Models\Role;
 use DB;
 class RoleController extends CommonController
 {
