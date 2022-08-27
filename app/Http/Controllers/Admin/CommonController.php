9280ffad5504276bb2915b0fc22c653e2917252e
Lin Aung <jijihohococo@gmail.com>
2022-08-27 21:26:03 +0900

    fix

--- app/Http/Controllers/Admin/CommonController.php
+++ app/Http/Controllers/Admin/CommonController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Traits\AdminRolePermission;
-class CommonController extends Controller
+abstract class CommonController extends Controller
 {
     //
     use AdminRolePermission;
