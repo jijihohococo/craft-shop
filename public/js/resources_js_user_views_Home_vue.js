90f0b84e6df87a593001a5a69110685ff2951298
Lin Aung <jijihohococo@gmail.com>
2022-09-24 16:30:25 +0900

    add item review js

--- public/js/resources_js_user_views_Home_vue.js
+++ public/js/resources_js_user_views_Home_vue.js
@@ -1498,9 +1498,44 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "showPromotionPercentage": () => (/* binding */ showPromotionPercentage)
 /* harmony export */ });
 function showAveragePercent(arr) {
-  return arr.reduce(function (a, b) {
-    return a + b;
-  }, 0) / arr.length / 5 * 100;
+  var oneStarCount = 0;
+  var twoStarCount = 0;
+  var threeStarCount = 0;
+  var fourStarCount = 0;
+  var fiveStarCount = 0;
+  var countItem = 0;
+  arr.map(function (a) {
+    switch (a) {
+      case "1":
+        oneStarCount++;
+        break;
+
+      case "2":
+        twoStarCount++;
+        break;
+
+      case "3":
+        threeStarCount++;
+        break;
+
+      case "4":
+        fourStarCount++;
+        break;
+
+      case "5":
+        fiveStarCount++;
+        break;
+    }
+
+    countItem++;
+  });
+  var oneStarPercent = oneStarCount / countItem * 100;
+  var twoStarPercent = twoStarCount / countItem * 100;
+  var threeStarPercent = threeStarCount / countItem * 100;
+  var fourStarPercent = fourStarCount / countItem * 100;
+  var fiveStarPercent = fiveStarCount / countItem * 100;
+  var averageStarData = oneStarPercent > 0 || twoStarPercent > 0 || threeStarPercent > 0 || fourStarPercent > 0 || fiveStarPercent > 0 ? (5 * fiveStarCount + 4 * fourStarCount + 3 * threeStarCount + 2 * twoStarCount + 1 * oneStarCount) / (fiveStarCount + fourStarCount + threeStarCount + twoStarCount + oneStarCount) : 0;
+  return averageStarData / 5 * 100;
 }
 function showPrice(price) {
   return parseInt(price).toString();
--- resources/js/user/helpers/general.js
+++ resources/js/user/helpers/general.js
@@ -1,5 +1,51 @@
 export function showAveragePercent(arr) {
-	return ((arr.reduce((a, b) => a + b, 0) / arr.length)/5)*100;
+	 var oneStarCount=0
+	 var twoStarCount=0
+	 var threeStarCount=0
+	 var fourStarCount=0
+	 var fiveStarCount=0
+	 var countItem=0
+	arr.map( (a) => {
+		switch(a){
+			case "1":
+			oneStarCount++;
+			break
+			case "2":
+			twoStarCount++;
+			break;
+			case "3":
+			threeStarCount++;
+			break;
+			case "4":
+			fourStarCount++;
+			break;
+			case "5":
+			fiveStarCount++;
+			break;
+		}
+		countItem++
+	} )
+	var oneStarPercent=(oneStarCount/countItem)*100
+	var twoStarPercent=(twoStarCount/countItem)*100
+	var threeStarPercent=(threeStarCount/countItem)*100
+	var fourStarPercent=(fourStarCount/countItem)*100
+	var fiveStarPercent=(fiveStarCount/countItem)*100
+
+	var averageStarData=(oneStarPercent >0 ||
+		twoStarPercent >0 ||
+		threeStarPercent > 0 ||
+		fourStarPercent > 0 ||
+		fiveStarPercent > 0 ) ? 
+	(5*fiveStarCount + 
+	4*fourStarCount +
+	3*threeStarCount +
+	2*twoStarCount +
+	1*oneStarCount)/(fiveStarCount+
+	fourStarCount+
+	threeStarCount+
+	twoStarCount+oneStarCount) : 0;
+	return (averageStarData/5)*100
+
 }
 export function showPrice(price){
 	return parseInt(price).toString()
