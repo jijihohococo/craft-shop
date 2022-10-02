ff394c63de60a8291fd1bc6e5ebfaeafaedd2878
Lin Aung <jijihohococo@gmail.com>
2022-10-02 10:01:13 +0900

    refactor

--- app/Http/Controllers/User/Shop/ItemController.php
+++ app/Http/Controllers/User/Shop/ItemController.php
@@ -61,8 +61,8 @@ class ItemController extends Controller
         }
         return response()->json([
             'items' => empty($items) ? $items : $items->latest('id')->paginate(10) ,
-            'max_price' => $this->item->getMaxPrice($itemIds) ,
-            'min_price' => $this->item->getMinPrice($itemIds)
+            'max_price' => $this->item->getMaxPrice([]) ,
+            'min_price' => $this->item->getMinPrice([])
         ]);
     }
 }
