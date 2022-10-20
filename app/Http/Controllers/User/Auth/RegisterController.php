3205ee57dba200e808cc2dad1f1a2cc80388764f
Lin Aung <jijihohococo@gmail.com>
2022-10-20 12:17:26 +0900

    add transactions after user registration and login

--- app/Http/Controllers/User/Auth/RegisterController.php
+++ app/Http/Controllers/User/Auth/RegisterController.php
@@ -5,10 +5,19 @@ namespace App\Http\Controllers\User\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Repositories\{ShoppingCartRepositoryInterface,WishListRepositoryInterface};
 use DB;
 class RegisterController extends Controller
 {
     //
+
+    private $shoppingCart , $wishList ;
+
+    public function __construct(ShoppingCartRepositoryInterface $shoppingCart,WishListRepositoryInterface $wishList){
+        $this->shoppingCart=$shoppingCart;
+        $this->wishList=$wishList;
+    }
+
     private function validateData(){
         return [
             'name' => ['required', 'string', 'max:100', 'unique:users' ] ,
@@ -21,7 +30,10 @@ class RegisterController extends Controller
         try{
             $request->validate($this->validateData());
             DB::beginTransaction();
-            $token=User::create($request->all())->passportToken();
+            $user=User::create( $request->all() );
+            $token=$user->passportToken();
+            $this->shoppingCart->update( (string) $user->id);
+            $this->wishList->update( (string) $user->id );
             DB::commit();
             return getToken(['access_token' => User::ACCESS_TOKEN ,
                 'refresh_token' => User::REFRESH_TOKEN  ],$token,'Registration is Success');
