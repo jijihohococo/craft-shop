063bb579b0da2e6ce2310d4609cd48ce1f4fd175
Lin Aung <jijihohococo@gmail.com>
2022-10-26 11:48:17 +0900

    refactor

--- resources/js/admin/views/Promotion.vue
+++ resources/js/admin/views/Promotion.vue
@@ -100,87 +100,23 @@
 		</section>
 	</template>
 	<script >
-		import Pagination from '../../components/Pagination';
-
-		import Delete from '../components/Delete';
-
-		import DeleteAllCheck from '../components/DeleteAllCheck';
-
-		import ContentHeader from '../components/ContentHeader';
-
-		import CreateButton from '../components/CreateButton';
-
-		import EditButton from '../components/EditButton';
-
-		import ViewButton from '../components/ViewButton';
-
-		import Error from '../components/Error';
-
-		import DeleteCheck from '../components/DeleteCheck';
-
-		import Trash from '../components/Trash';
-
-		import DeleteMultiple from '../components/DeleteMultiple';
-
-		import Search from '../components/Search';
-
-		import { errorResponse , checkContentPermission , makeSelect , makeRoute , checkActions , deleteFromArray , unauthorizedActions , showPageNumber } from '../helpers/check';
+		import { errorResponse , checkContentPermission , makeRoute , showPageNumber } from '../helpers/check';
 
 		import { showSwalLoading } from  '../../helpers/general'
+
+		import { mixin } from '../common/data_list';
 		
 		export default {
-			components: {
-				Search,
-				Pagination,
-				ContentHeader,
-				Delete,
-				CreateButton,
-				EditButton,
-				ViewButton,
-				Error,
-				DeleteCheck,
-				Trash,
-				DeleteMultiple,
-				DeleteAllCheck
-			},
 			data () {
 				return {
 					content : 'Promotion',
-					deleteData : [],
-					multipleData : [] ,
-					promotions : {},
-					search : null ,
-					currentPage : 1 ,
-					actions : {
-						create : '' ,
-						read : '' ,
-						update : '' ,
-						delete : ''
-					}
+					promotions : {}
 				}
 			},
+			mixins: [mixin],
 			methods :{
 				freshPage(){
 					this.getPromotions( showPageNumber(this.currentPage) )
-				},
-				checkAuthorizeActions(actions){
-					return checkActions(actions);
-				},
-				checkUnauthorizeActions(actions){
-					return unauthorizedActions(actions);
-				},
-				selectChecks(){
-					if(this.$refs.deleteCheck!==undefined){
-						makeSelect(this.$refs.deleteCheck,true)
-					}
-				},
-				cancelChecks(){
-					if(this.$refs.deleteCheck!==undefined){
-						makeSelect(this.$refs.deleteCheck,false)
-					}
-				},
-				updateData(object){
-					deleteFromArray(this.promotions.data,object)
 				},
 				getPromotions(page){
 					window.axios.get(makeRoute(this,page,'promotion') + page ).then(( response ) =>  {
--- resources/js/admin/views/Subcategory.vue
+++ resources/js/admin/views/Subcategory.vue
@@ -95,87 +95,23 @@
 </template>
 <script >
 
-    import Pagination from '../../components/Pagination';
-
-    import Delete from '../components/Delete';
-
-    import DeleteAllCheck from '../components/DeleteAllCheck';
-
-    import ContentHeader from '../components/ContentHeader';
-
-    import CreateButton from '../components/CreateButton';
-
-    import EditButton from '../components/EditButton';
-
-    import ViewButton from '../components/ViewButton';
-
-    import Error from '../components/Error';
-
-    import DeleteCheck from '../components/DeleteCheck';
-
-    import Trash from '../components/Trash';
-
-    import DeleteMultiple from '../components/DeleteMultiple';
-
-    import Search from '../components/Search';
-
-    import { errorResponse , checkContentPermission , makeSelect , makeRoute , checkActions , deleteFromArray , unauthorizedActions , showPageNumber } from '../helpers/check';
+    import { errorResponse , checkContentPermission , makeRoute , showPageNumber } from '../helpers/check';
 
     import { showSwalLoading } from  '../../helpers/general'
 
+    import { mixin } from '../common/data_list';
+
     export default {
-        components: {
-            Search,
-            Pagination,
-            ContentHeader,
-            Delete,
-            CreateButton,
-            EditButton,
-            ViewButton,
-            Error,
-            DeleteCheck,
-            Trash,
-            DeleteMultiple,
-            DeleteAllCheck
-        },
         data () {
            return {
             content : 'Subcategory',
-            deleteData : [],
-            multipleData : [] ,
-            subcategories : {},
-            currentPage : 1 ,
-            search : null ,
-            actions : {
-                create : '' ,
-                read : '' ,
-                update : '' ,
-                delete : ''
-            }
+            subcategories : {}
         }
     },
+    mixins: [mixin],
     methods :{
         freshPage(){
             this.getSubcategories( showPageNumber(this.currentPage) )
-        },
-        checkAuthorizeActions(actions){
-            return checkActions(actions);
-        },
-        checkUnauthorizeActions(actions){
-            return unauthorizedActions(actions);
-        },
-        selectChecks(){
-            if(this.$refs.deleteCheck!==undefined){
-                makeSelect(this.$refs.deleteCheck,true)
-            }
-        },
-        cancelChecks(){
-            if(this.$refs.deleteCheck!==undefined){
-                makeSelect(this.$refs.deleteCheck,false)
-            }
-        },
-        updateData(object){
-            deleteFromArray(this.subcategories.data,object)
         },
         getSubcategories(page){
             window.axios.get(makeRoute(this,page,'subcategory') + page ).then(( response ) =>  {
