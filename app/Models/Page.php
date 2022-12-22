4ad89797ba6e8ecf25830c3c6411f89e142aecee
Lin Aung <jijihohococo@gmail.com>
2022-12-22 16:01:29 +0900

    add

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -3,10 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\SoftDeletes;
-use App\Traits\{SearchNameTrait,DeleteSearch};
+use App\Traits\{SearchNameTrait,DeleteSearch,SeoTrait};
 class Page extends TransactionModel
 {
-    use SoftDeletes,SearchNameTrait,DeleteSearch;
+    use SoftDeletes,SearchNameTrait,DeleteSearch,SeoTrait;
 
     protected $fillable=[
         'name',
