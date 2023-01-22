e25fc3bdf468677ba789b11d60d9d51a9f7b60f1
Lin Aung <jijihohococo@gmail.com>
2023-01-22 14:30:07 +0900

    add seo

--- app/Http/Controllers/User/FaqController.php
+++ app/Http/Controllers/User/FaqController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Faq;
+use App\Models\{Faq,Seo};
 class FaqController extends Controller
 {
     //
     public function index(){
         return response()->json([
-            'faqs' => (new Faq)->getAll()
+            'faqs' => (new Faq)->getAll(),
+            'seo' => Seo::getSeo('Faq',1)
         ]);
     }
 }
